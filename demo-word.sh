fb815b563a74aad29442ca9d129f39bcde66bedc
ApoorvaNITS <51491462+ApoorvaNITS@users.noreply.github.com>
2019-06-08 12:49:09 +0530

    Update demo-word.sh

--- demo-word.sh
+++ demo-word.sh
@@ -1,4 +1,4 @@
 make
 
-time ./word2vec -train Medline -output vectors.txt -cbow 1 -size 128 -window 30 -negative 10 -hs 0 -threads 100 -binary 0 -iter 15
+time ./word2vec -train Medline -output vectors.txt -cbow 1 -size 128 -window 30 -negative 10 -hs 0 -threads 1000 -binary 0 -iter 15
 ./distance vectors.txt
